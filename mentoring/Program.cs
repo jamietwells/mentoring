6ba70018c67c86553300acd0c70007625bee80ca
Jamie Twells <jtwells3@googlemail.com>
2018-03-29 09:14:47 +0100

    task description added

--- mentoring/Program.cs
+++ mentoring/Program.cs
@@ -9,4 +9,20 @@ namespace Mentoring
             Console.WriteLine("Hello World!");
         }
     }
+
+    /*
+        Task 1: Create a program to find all the factors of any positive integer.
+
+        For example:
+
+            Input: 12
+            Output: 1, 2, 3, 4, 6, 12
+
+            Input: 7
+            Output: 1, 7
+
+            Input: 16
+            Output: 1, 2, 4, 8, 16
+
+     */
 }
