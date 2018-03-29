26bb1f0ba8abdd09f1b8988daaae6de3deca8540
Michael DeLuca (Admin) <mdeluca.admin@office.zen.co.uk>
2018-03-29 16:26:03 +0100

    wrote program

--- mentoring/Program.cs
+++ mentoring/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Mentoring
 {
@@ -6,9 +7,61 @@ namespace Mentoring
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter your number:");
+            string input = Console.ReadLine();
+            Console.WriteLine("Your number : " + input);
+            int inputconv = Convert.ToInt32(input);
+           var factorcalc = new FactorsCalculator(inputconv);
+           
+           foreach(var factor in factorcalc.GetFactors())
+           {
+               Console.WriteLine(factor);
+               
+           } 
+
+           //Console.WriteLine(factorcalc.GetFactors());
+
+
+
         }
+
+
+
     }
+
+    class FactorsCalculator
+    {
+        int _input;
+
+        public FactorsCalculator(int input)
+        {
+            this._input = input;
+        }
+
+        public List<int> GetFactors()
+        {
+            var FactorsList = new List<int>();
+
+            for (int i = 1; i <= _input; i = i + 1)
+            {
+
+                if (_input % i == 0)
+                {
+                    FactorsList.Add(i);
+                }
+            }
+
+            return FactorsList;
+
+        }
+
+
+    }
+
+
+
+
 
     /*
         Task 1: Create a program to find all the factors of any positive integer.
