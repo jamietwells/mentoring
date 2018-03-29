36aede3090ced4d8c5f3de8c48fa3a92d9d68a6f
Jamie Twells <jtwells3@googlemail.com>
2018-03-29 08:51:54 +0100

    Add project files.

---
+++ mentoring/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mentoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
